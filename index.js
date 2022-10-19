f19417125043115cb9c944265423f9b913b582b6
Pranay Kothari <pranay.kothari109@gmail.com>
2022-10-19 16:11:57 +0530

    added routes for userRouter and resultRouter

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const express = require('express');
 const cors = require('cors');
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
+const userRouter = require('./routes/userRouter');
+const resultRouter = require('./routes/resultRouter');
 
 dotenv.config();
 
@@ -9,6 +11,18 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
+app.get('/', async (req, res) => {
+    res.send('Tailweb Backend');
+})
+
+
+// Routes
+app.use('/users', userRouter);
+app.use('/result', resultRouter);
+
+
+
+// MongoDB connection
 const PORT = process.env.PORT || 5000;
 const URL = process.env.DB_URL;
 
